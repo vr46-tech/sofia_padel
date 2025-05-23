80028974f0e108dd543b5e9514284f949032639f
vr46-tech <francois.dehaibe@flexconsulting.eu>
2025-05-23 15:12:19 +0200

    Update Invoice.js

--- pages/api/Invoice.js
+++ pages/api/Invoice.js
@@ -40,6 +40,18 @@ async function getNextInvoiceNumber() {
 }
 
 export default async function handler(req, res) {
+
+   if (req.method === 'OPTIONS') {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(204).end();
+  return;
+}
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method Not Allowed" });
     return;
