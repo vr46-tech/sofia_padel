7e27b9bd1ef362d4dbefb6aa8b1ce770512c0e69
vr46-tech <francois.dehaibe@flexconsulting.eu>
2025-05-28 13:30:55 +0200

    Update Products.js

--- pages/api/Products.js
+++ pages/api/Products.js
@@ -20,7 +20,7 @@ const db = getFirestore(firebaseApp);
 
 function setCORSHeaders(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
 
